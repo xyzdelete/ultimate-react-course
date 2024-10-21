8267bed63724e9f89ebeeadd0eec6b7b4d6559de
Arturs Anikins <xyzdelete@protonmail.com>
2024-10-21 17:05:26 +0300

    48. Conditional Rendering With Ternaries

--- 03-pizza-menu/my/pizza-menu/src/index.js
+++ 03-pizza-menu/my/pizza-menu/src/index.js
@@ -76,12 +76,14 @@ function Menu() {
     <main className="menu">
       <h2>Our menu </h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu.Please come back later</p>
       )}
 
       {/* <Pizza
@@ -130,12 +132,16 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           {" "}
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}::00 and {closeHour}::00
+        </p>
       )}
     </footer>
   );
