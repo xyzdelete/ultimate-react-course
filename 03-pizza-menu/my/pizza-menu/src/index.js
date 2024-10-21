50df1a056179c76b068d04bfd2af86a2e546cc0e
Arturs Anikins <xyzdelete@protonmail.com>
2024-10-21 14:38:26 +0300

    42. Passing and Receiving Props

--- 03-pizza-menu/my/pizza-menu/src/index.js
+++ 03-pizza-menu/my/pizza-menu/src/index.js
@@ -72,10 +72,35 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu </h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 
@@ -95,16 +120,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()}We're currently open
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
