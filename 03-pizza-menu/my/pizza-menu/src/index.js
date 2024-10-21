d3ba633db35d79e3f1500c67e71333a2cc5f92ed
Arturs Anikins <xyzdelete@protonmail.com>
2024-10-21 13:42:51 +0300

    39. JavaScript Logic in Components

--- 03-pizza-menu/my/pizza-menu/src/index.js
+++ 03-pizza-menu/my/pizza-menu/src/index.js
@@ -49,12 +49,41 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu </h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  console.log(hour);
+
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed!");
+
+  return <footer>{new Date().toLocaleTimeString()}We're currently open</footer>;
 }
 
 function Pizza() {
