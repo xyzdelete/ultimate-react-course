db2b48ad2df8493989629f28c8c949f03559ea31
Arturs Anikins <xyzdelete@protonmail.com>
2024-10-22 15:32:56 +0300

    67. A Vanilla JavaScript Implementation

--- 04-steps/my/steps/src/App.js
+++ 04-steps/my/steps/src/App.js
@@ -7,6 +7,15 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+export function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -23,7 +32,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -55,6 +64,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
