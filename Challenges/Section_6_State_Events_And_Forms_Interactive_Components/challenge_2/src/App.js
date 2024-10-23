948b9ffe062a7125e4186555bb338b4d882799d5
Arturs Anikins <xyzdelete@protonmail.com>
2024-10-23 23:52:29 +0300

    93. CHALLENGE #1: Tip Calculator
    My Version

--- Challenges/Section_6_State_Events_And_Forms_Interactive_Components/challenge_2/src/App.js
+++ Challenges/Section_6_State_Events_And_Forms_Interactive_Components/challenge_2/src/App.js
@@ -2,75 +2,82 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [bill, setBill] = useState(0);
+  const [myTip, setMyTip] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  function handleResetButton() {
+    setBill(0);
+    setMyTip(0);
+    setFriendTip(0);
+    setTotal(0);
+    window.location.reload();
+  }
+
   return (
     <div className="App">
-      <Counter />
+      <Bill
+        myTip={myTip}
+        friendTip={friendTip}
+        setTotal={setTotal}
+        setBill={setBill}
+        onChange={setTotal}
+      >
+        How much was the bill?
+      </Bill>
+      <Service setTotal={setTotal} onChange={setMyTip}>
+        How did you like the service?
+      </Service>
+      <Service setTotal={setTotal} onChange={setFriendTip}>
+        How did your friend like the service?
+      </Service>
+
+      <Output total={total} bill={bill} myTip={myTip} friendTip={friendTip} />
+
+      <button onClick={handleResetButton}>Reset</button>
     </div>
   );
 }
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+function Output({ total, bill, myTip, friendTip }) {
+  return (
+    <h1>{`You pay $${total} ($${bill} + $${(myTip + friendTip) / 2} tip)`}</h1>
+  );
+}
 
-  const date = new Date();
-  date.setDate(date.getDate() + count);
-
-  function handleSliderChange(e) {
-    setStep(Number(e.target.value));
-  }
-
-  function handleReset() {
-    setCount(0);
-    setStep(1);
-  }
-
-  function handleIncrement() {
-    setCount((c) => c + step);
-  }
-
-  function handleDecrement() {
-    setCount((c) => c - step);
+function Bill({ setTotal, children, onChange, setBill, myTip, friendTip }) {
+  function handleBillInput(e) {
+    e.preventDefault();
+    setBill(Number(e.target.value));
+    onChange(Number(e.target.value));
+    setTotal(Number(e.target.value) + (Number(myTip) + Number(friendTip)) / 2);
   }
 
   return (
     <div>
-      <div>
-        <input
-          type="range"
-          min="0"
-          max="10"
-          value={step}
-          onChange={(e) => handleSliderChange(e)}
-        />
-        <span>{step}</span>
-      </div>
+      <span>{children}</span>{" "}
+      <input placeholder="0" type="text" onChange={(e) => handleBillInput(e)} />
+    </div>
+  );
+}
 
-      <div>
-        <button onClick={handleDecrement}>-</button>
-        <input
-          type="text"
-          onChange={(e) => setCount(Number(e.target.value))}
-          placeholder={count}
-          value={count}
-        />
-        <button onClick={handleIncrement}>+</button>
-      </div>
+function Service({ children, onChange, setTotal }) {
+  function handleSelect(e) {
+    e.preventDefault();
+    onChange(Number(e.target.value));
+    setTotal((total) => total + Number(e.target.value) / 2);
+  }
 
-      <p>
-        <span>
-          {count === 0
-            ? "Today is "
-            : count > 0
-            ? `${count} days from today is `
-            : `${Math.abs(count)} days ago was `}
-        </span>
-        <span>{date.toDateString()}</span>
-      </p>
-
-      {count !== 0 || step !== 1 ? (
-        <button onClick={handleReset}>Reset</button>
-      ) : null}
+  return (
+    <div>
+      <span>{children}</span>
+      <select onChange={(e) => handleSelect(e)}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
     </div>
   );
 }
